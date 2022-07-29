b8c8137f673ad5b0e90720d1d9e099ef733abac4
Andrea Garcia <andy.abgm@gmail.com>
2022-07-28 20:49:46 -0500

    primer commmit

---
+++ script.js
@@ -0,0 +1,63 @@
+let searchBtn= document.getElementById("search-btn");
+let countryInp= document.getElementById("country-inp");
+searchBtn.addEventListener("click", () =>{
+    let countryName= countryInp.value;
+    let finalURL= `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    // console.log(finalURL)
+    fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+        // console.log (data[0]);
+        // console.log(data[0].capital[0]);
+        // console.log(data[0].flgs.svg);
+        // console.log(data[0].name.common);
+        // console.log(data[0].continents[0]);
+        // console.log(Object.keys(data[0].currencies)[0]);
+        // console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+        // console.log(
+        // Object.values(data[0].languages).toString().split(",").join(",")
+        result.innerHTML= `
+            <img src="${data[0].flags.svg}"class="flag-img">
+            <h2>${data[0].name.common}</h2>
+            <div class="wrapper">
+                <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${data[0].capital[0]}</span>
+                </div>
+            </div>
+            
+            <div class="wrapper"> 
+                <div class="data-wrapper">
+                <h4>Continente:</h4>
+                <span>${data[0].continents[0]}</span>
+                </div>
+            </div>
+            <div class="wrapper"> 
+                <div class="data-wrapper">
+                <h4>Población:</h4>
+                <span>${data[0].population}</span>
+                </div>
+            </div>
+            <div class="wrapper"> 
+                <div class="data-wrapper">
+                <h4>Divisa:</h4>
+                <span> ${data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies)[0]}</span>
+                </div>
+            </div>
+            <div class="wrapper"> 
+                <div class="data-wrapper">
+                <h4>Lenguaje:</h4>
+                <span> ${Object.values(data[0].languages).toString().split(",").join(",")}</span>
+                </div>
+            </div>
+    `;
+    })
+    .catch((error) => {
+        if (countryName.lengh == 0){
+            result.innerHTML=`<h3>The input field cannot be empty</h3>`;
+        } else {
+        result.innerHTML= `<h3>Por favor ingresa un nombre válido...</h3>`;
+        }
+        // console.log(error);
+    })
+})
